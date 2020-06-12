141f42c352cbea2103b0a68633d76e67003f7b4d
Hilderlan <hilakt9698@gmail.com>
2020-06-12 12:45:03 -0300

    Global styles added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import GlobalStyles from './styles/global';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <GlobalStyles />
       <h1>Hello World!</h1>
-    </div>
+    </>
   );
 }
 
